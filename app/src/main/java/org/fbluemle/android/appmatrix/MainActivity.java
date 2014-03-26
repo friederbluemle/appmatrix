b124a0ed0ed92afc6cf36e425fd6ba60b6336644
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-03-25 23:42:22 -0700

    Display simple ListView on MainActivity

--- app/src/main/java/org/fbluemle/android/appmatrix/MainActivity.java
+++ app/src/main/java/org/fbluemle/android/appmatrix/MainActivity.java
@@ -1,24 +1,77 @@
 package org.fbluemle.android.appmatrix;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.fbluemle.android.appmatrix.adapters.AppAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    protected ListAdapter mAdapter;
+    protected ListView mList;
+
+    static final String[] FRUITS = new String[]{"Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mList = (ListView) findViewById(R.id.listView);
+        mList.setAdapter(new AppAdapter(this, FRUITS));
+
+        mList.setTextFilterEnabled(true);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private String[] getPackages() {
+        List<String> packagesStr = new ArrayList<String>();
+
+        final PackageManager pm = getPackageManager();
+//get a list of installed apps.
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            Log.d(TAG, "Installed package :" + packageInfo.packageName);
+            packagesStr.add(packageInfo.packageName);
+            Log.d(TAG, "Source dir : " + packageInfo.sourceDir);
+            Log.d(TAG, "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+        }
+        return Arrays.copyOf(packagesStr.toArray(), packagesStr.size(), String[].class);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -33,6 +86,8 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
+        } else if (id == R.id.action_refresh) {
+            mList.setAdapter(new AppAdapter(this, getPackages()));
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/org/fbluemle/android/appmatrix/adapters/AppAdapter.java
@@ -0,0 +1,53 @@
+package org.fbluemle.android.appmatrix.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.fbluemle.android.appmatrix.R;
+
+/**
+ * Created by Frieder on 3/25/2014.
+ */
+public class AppAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+
+    public AppAdapter(Context context, String[] values) {
+        super(context, R.layout.list_app, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.list_app, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
+        textView.setText(values[position]);
+
+        // Change icon based on name
+        String s = values[position];
+
+        System.out.println(s);
+
+        if (s.equals("WindowsMobile")) {
+            imageView.setImageResource(R.drawable.ic_launcher);
+        } else if (s.equals("iOS")) {
+            imageView.setImageResource(R.drawable.ic_launcher);
+        } else if (s.equals("Blackberry")) {
+            imageView.setImageResource(R.drawable.ic_launcher);
+        } else {
+            imageView.setImageResource(R.drawable.ic_launcher);
+        }
+
+        return rowView;
+    }
+}
